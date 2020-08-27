6274571c923336fa6ba76d7f8224a4d11c9cf124
Michael Richardson <mrichardson03@gmail.com>
2020-08-27 12:01:11 -0400

    fix: another test fix

--- plugins/modules/test_module.py
+++ plugins/modules/test_module.py
@@ -3,8 +3,6 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-# This is a test fix.
-
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
     'status': ['preview'],
